4255bfda090a0f497054422bc7508e59a9e4760d
Déborah <dsales15@gmail.com>
2024-03-18 20:24:50 -0300

    first commit

---
+++ test/resultados_tjse_test.dart
@@ -0,0 +1,8 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:resultados_tjse/resultados_tjse.dart';
+
+void main() {
+  test('criar widget de resultados', () {
+    const Resultado('Titulo um', 'Titulo dois', 'Dados', 'Dados Expandidos');
+  });
+}
